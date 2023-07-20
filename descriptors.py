a3d581e4ebfd7e4204e1408fd107d0053d60424b
AdskiyPonchik <tsoy507@gmail.com>
2023-07-20 12:51:06 +0200

    new file:   .idea/Perfectisation.iml 	modified:   .idea/misc.xml 	deleted:    .idea/vcs.xml 	new file:   descriptors.py

---
+++ descriptors.py
@@ -0,0 +1,39 @@
+class ReadIntX:
+    def __set_name__(self, owner, name):
+        self.name = "_x"
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name)
+
+
+class Integer:  # data-descriptor
+    @classmethod
+    def verify_coord(cls, coord):
+        if type(coord) != int:
+            raise TypeError("Coord should be integer")
+
+    def __set_name__(self, owner, name):
+        self.name = "_" + name
+
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.name]
+
+    def __set__(self, instance, value):
+        self.verify_coord(value)
+        setattr(instance, self.name, value)  # instance.__dict__[self.name] = value - alternative
+
+
+class Point3D:
+    x = Integer()
+    y = Integer()
+    z = Integer()
+    xr = ReadIntX()
+
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+
+pt = Point3D(1, 2, 3)
+print(pt.xr, pt.__dict__)
